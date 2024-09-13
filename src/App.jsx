142875afa148bbdc582e502ddd0d9c79aad2346e
vansh kakran <vanshkakran82@gmail.com>
2024-09-13 20:14:35 +0530

    jj

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,55 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Home from "./Home/Home";
-import { BrowserRouter, Routes, Route , Link } from "react-router-dom";
+import { HashRouter, Routes, Route, } from "react-router-dom";
 import FloarTiles from "./NavigationComponents/FloarTiles";
 import WallTiles from "./NavigationComponents/WallTiles";
-import Aboutus from './NavigationComponents/Aboutus'
+import Aboutus from "./NavigationComponents/Aboutus";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+//   const router = createBrowserRouter([
+//     {
+//       path:"/",
+//       element:<Home/>,
+
+//   },
+
+//   {
+//     path:"/walltiles",
+//     element:<WallTiles/>,
+
+// }
+// ,
+// {
+//   path:"/floartiles",
+//   element:<FloarTiles/>,
+
+// },
+// {
+//   path:"/aboutus",
+//   element:<Aboutus/>,
+
+// },
+
+
+// ])
   return (
     <div>
-      <BrowserRouter>
+      
+      <HashRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/home" element={<Home/>}></Route>
-        <Route path="/walltiles" element={<WallTiles/>}></Route>
-        <Route path="/floartiles" element={<FloarTiles/>}></Route>
-        <Route path="/aboutus" element={<Aboutus/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/walltiles" element={<WallTiles />}></Route>
+        <Route path="/floartiles" element={<FloarTiles />}></Route>
+        <Route path="/aboutus" element={<Aboutus />}></Route>
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
       
-    
+       {/* <Navbar/>
+       <RouterProvider router={router}/> */}
     </div>
   );
 }
